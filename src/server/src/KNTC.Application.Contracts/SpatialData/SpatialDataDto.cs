5ad10dfdc0d1e54e60e555acf0f7381e5cea804d
Duong DX <duongdx@gmail.com>
2023-03-18 19:09:16 +0700

    dsa

--- src/server/src/KNTC.Application.Contracts/SpatialData/SpatialDataDto.cs
+++ src/server/src/KNTC.Application.Contracts/SpatialData/SpatialDataDto.cs
@@ -8,9 +8,9 @@ public class SpatialDataDto : EntityDto<int>
 {
     //public int Id { get; }
     public Double OBJECTID { get; }
-    public string? TenToChuc { get; }
-    public Single? Quyen { get; }
-    public string? So_to_BD { get; }
+    public string TenToChuc { get; }
+    public Single Quyen { get; }
+    public string So_to_BD { get; }
     public SqlGeometry geometry { get; set; }
     public string geoJson { get; }
 }
