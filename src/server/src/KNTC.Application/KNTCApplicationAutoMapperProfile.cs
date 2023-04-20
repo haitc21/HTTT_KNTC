6e2b86522c8fcc1cd3ce6d3ca7f3d30227b155cd
haitc <hai.tc21@gmail.com>
2023-04-20 22:20:42 +0700

    fix denounce

--- src/server/src/KNTC.Application/KNTCApplicationAutoMapperProfile.cs
+++ src/server/src/KNTC.Application/KNTCApplicationAutoMapperProfile.cs
@@ -55,7 +55,7 @@ public class KNTCApplicationAutoMapperProfile : Profile
         CreateMap<Denounce, DenounceDto>();
         CreateMap<Denounce, DenounceInListDto>();
         CreateMap<Denounce, DenounceExcelDto>()
-                 .ForMember(dto => dto.KetQua, opt => opt.MapFrom(denounce => denounce.KetQua.ToVNString()));
+                 .ForMember(dto => dto.KetQua, opt => opt.MapFrom(denounce => denounce.KetQua.Value.ToVNString()));
         CreateMap<CreateDenounceDto, Denounce>();
         CreateMap<UpdateDenounceDto, Denounce>();
 
