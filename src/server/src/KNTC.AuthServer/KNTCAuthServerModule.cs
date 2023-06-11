0cd3b02f533ddc0b9da3063f6255131b345468c1
haitc <hai.tc21@gmail.com>
2023-06-11 20:43:13 +0700

    deploy

--- src/server/src/KNTC.AuthServer/KNTCAuthServerModule.cs
+++ src/server/src/KNTC.AuthServer/KNTCAuthServerModule.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Serilog;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
@@ -78,20 +79,19 @@ public class KNTCAuthServerModule : AbpModule
                 options.AddDevelopmentEncryptionAndSigningCertificate = true;
             });
         }
-
-        // Production or Staging environment
+        //    Production or Staging environment
         if (!hostingEnvironment.IsDevelopment())
         {
-            //PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
-            //{
-            //    options.AddDevelopmentEncryptionAndSigningCertificate = false;
-            //});
+            PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
+            {
+                options.AddDevelopmentEncryptionAndSigningCertificate = false;
+            });
 
-            //PreConfigure<OpenIddictServerBuilder>(builder =>
-            //{
-            //    builder.AddSigningCertificate(GetSigningCertificate(hostingEnvironment));
-            //    builder.AddEncryptionCertificate(GetSigningCertificate(hostingEnvironment));
-            //});
+            PreConfigure<OpenIddictServerBuilder>(builder =>
+            {
+                builder.AddSigningCertificate(GetSigningCertificate(hostingEnvironment));
+                builder.AddEncryptionCertificate(GetSigningCertificate(hostingEnvironment));
+            });
 
             // disable https
             //Configure<OpenIddictServerAspNetCoreBuilder>(configure =>
@@ -101,10 +101,14 @@ public class KNTCAuthServerModule : AbpModule
         }
     }
 
-    //private X509Certificate2 GetSigningCertificate(IWebHostEnvironment hostingEnv)
-    //{
-    //    return new X509Certificate2(Path.Combine(hostingEnv.ContentRootPath, "authserver.pfx"), "HTTTKNTC@VNUA2023");
-    //}
+    private X509Certificate2 GetSigningCertificate(IWebHostEnvironment hostingEnv)
+    {
+        string fileName = Path.Combine(hostingEnv.ContentRootPath, "authserver.pfx");
+        string password = "KNTC@Vnua2023";
+        Log.Information($"X509Certificate2 fileName: {fileName}");
+        Log.Information($"X509Certificate2 password: {password}");
+        return new X509Certificate2(fileName, password);
+    }
 
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
