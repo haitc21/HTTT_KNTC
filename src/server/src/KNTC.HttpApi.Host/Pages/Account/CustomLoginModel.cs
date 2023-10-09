a833d37f688b2bcdf13ffc50fafd5e6241a53ec8
haitc <hai.tc21@gmail.com>
2023-10-10 01:46:33 +0700

    uodate codenbe

---
+++ src/server/src/KNTC.HttpApi.Host/Pages/Account/CustomLoginModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Account.Web.Pages.Account;
+
+namespace KNTC.Pages.Account;
+
+public class CustomLoginModel : LoginModel
+{
+    public CustomLoginModel(
+    Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider schemeProvider,
+    Microsoft.Extensions.Options.IOptions<Volo.Abp.Account.Web.AbpAccountOptions> accountOptions,
+    Microsoft.Extensions.Options.IOptions<Microsoft.AspNetCore.Identity.IdentityOptions> identityOptions)
+        : base(schemeProvider, accountOptions, identityOptions)
+    {
+    }
+}
+
